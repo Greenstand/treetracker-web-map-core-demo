15795c9c9d551a87533339beadd6aea743a1b46d
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-04-20 09:38:44 -0400

    fix: hide  header

--- Navigation/Stacks.js
+++ Navigation/Stacks.js
@@ -5,7 +5,11 @@ const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
